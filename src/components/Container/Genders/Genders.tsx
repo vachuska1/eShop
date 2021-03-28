67272502cfbc55e81b9febbc9de7db897cdd9791
Alexander Vachuška <a.vachuska@email.cz>
2021-03-28 17:06:21 +0200

    Update Genders.tsx

--- src/components/Container/Genders/Genders.tsx
+++ src/components/Container/Genders/Genders.tsx
@@ -3,6 +3,8 @@ import "./Genders.less";
 
 export const Genders: React.FC = () => {
   return (
+    // pojmenovavej classy malyma pismenama, je to takovej standart "genders, genders__mens, genders__mens--name"
+    // podle me tyhle sekce budou asi stejny obe dve, tak proc mas rozdilny classy?
     <div className="Genders">
       <div className="Genders__Mens">
         <div className="Genders__Mens--name">Mens</div>
